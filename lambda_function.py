2db7a8d826fd4e0f925e67073cad8da85c691c1b
James Sungjae Lee <odobenuseKR@gmail.com>
2021-11-04 19:34:06 +0900

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,63 @@
+import os
+import json
+import base64
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from requests_toolbelt.multipart import decoder
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+model = load_model("/var/task/mobilenetv2")
+
+def multipart_to_input(multipart_data):
+    binary_content = []
+    for part in multipart_data.parts:
+        binary_content.append(part.content)
+
+    img = BytesIO(binary_content[0])
+    img = Image.open(img)
+    img = img.resize((224, 224), Image.ANTIALIAS)
+    img = np.array(img)
+    
+    # 1, 224, 224, 3
+    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+    img = preprocess_input(img)
+    return img
+
+def decode_predictions(preds, top=5):
+    with open('/var/task/imagenet_class_index.json') as f:
+        CLASS_INDEX = json.load(f)
+    results = []
+    for pred in preds:
+        top_indices = pred.argsort()[-top:][::-1]
+        result = [tuple(CLASS_INDEX[str(i)]) + (pred[i],) for i in top_indices]
+        result.sort(key=lambda x: x[2], reverse=True)
+        results.append(result)
+    return results
+
+def inference_model(img):
+    result = model.predict(img)
+    result = decode_predictions(result)[0]
+    result = [(img_class, label, str(round(acc * 100, 4)) + '%') for img_class, label, acc in result]
+    return result
+    
+def lambda_handler(event, context):
+    
+    body = event['body-json']
+    body = base64.b64decode(body)
+    
+    boundary = body.split(b'\r\n')[0]
+    boundary = boundary.decode('utf-8')
+    content_type = f"multipart/form-data; boundary={boundary}"
+    
+    multipart_data = decoder.MultipartDecoder(body, content_type)
+    
+    img = multipart_to_input(multipart_data)
+    result = inference_model(img)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f"{result[0][1]}&{result[0][2]}&{result[1][1]}&{result[1][2]}&{result[2][1]}&{result[2][2]}&{result[3][1]}&{result[3][2]}&{result[4][1]}&{result[4][2]}")
+    }
